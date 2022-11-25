e96464c4585473ce4b268ffb30ef4539152bcc41
MandeepMann <mandeep_mann30@hotmail.com>
2022-11-24 20:08:01 -0500

    changed js namespacing

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ portfolio.projects = () => {
     const hoverInfo = document.querySelectorAll()
 }
 
+// "Get In Touch" button in header that scrolls down to contact secion
 // https://stackoverflow.com/questions/24739126/scroll-to-a-specific-element-using-html
+portfolio.toContact = () => {
+    const contactScroll = document.querySelector('.toContact')
+    const contactSection = document.querySelector('#contact')
+
+    contactScroll.addEventListener('click', () => {
+        contactSection.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        })
+    })
+}
+
+// Chevron pointing down button that scrolls down to next section (about)
 portfolio.scrollDown = () => {
     const headerScroll = document.querySelector('.scrollDown')
     const aboutSection = document.querySelector('#about')
@@ -17,18 +32,13 @@ portfolio.scrollDown = () => {
             inline: "nearest"
         });
     })
-
-    // headerScroll.addEventListener('click', () => {
-    //     window.scrollTo({
-    //         top: 926,
-    //         left: 0,
-    //         behavior: "smooth"
-    //     });
-    // });
 };
+
+
 
 portfolio.init = () => {
     portfolio.scrollDown();
+    portfolio.toContact();
 }
 
 portfolio.init();
