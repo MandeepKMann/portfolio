d28d94cb3cc15bfedd71d14f39f95ac043be50f4
MandeepMann <mandeep_mann30@hotmail.com>
2022-11-24 19:00:57 -0500

    playing around with different js scroll stuff in header

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ portfolio.projects = () => {
     // Event listener for hover
     const hoverInfo = document.querySelectorAll()
 }
+
+// https://stackoverflow.com/questions/24739126/scroll-to-a-specific-element-using-html
+portfolio.scrollDown = () => {
+    const headerScroll = document.querySelector('.scrollDown')
+    const aboutSection = document.querySelector('#about')
+
+    headerScroll.addEventListener('click', () => {
+        aboutSection.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+    })
+
+    // headerScroll.addEventListener('click', () => {
+    //     window.scrollTo({
+    //         top: 926,
+    //         left: 0,
+    //         behavior: "smooth"
+    //     });
+    // });
+};
+
+portfolio.init = () => {
+    portfolio.scrollDown();
+}
+
+portfolio.init();
