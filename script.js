5d41e4e0b26b46582152d0753e9835c9801e794f
MandeepMann <mandeep_mann30@hotmail.com>
2022-11-25 23:16:30 -0500

    typing js FINALLY works

--- script.js
+++ script.js
@@ -20,12 +20,61 @@ portfolio.stickyHeader = () => {
 }
 
 // Typewriter animation on header text
+// https://www.youtube.com/watch?v=R846J8LJ6os
 portfolio.typewriter = () => {
-    const titles = ['a Developer', 'an Educator', 'a Researcher'];
-    const typewriter = document.querySelector('#typewwriter');
+    // sleep function to be called in my async function to help my it pause between each letter (miliseconds)
+    const sleep = (ms) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
     
+    // List of titles to be typed out
+    const titles = ['a Developer', 'an Educator', 'a Researcher'];
+    const typewriter = document.getElementById('typewriter');
 
+    // amount of time i want to sleep between each letter in miliseconds (ms)
+    let sleepTime = 150;
 
+    // Current index for the word I want to start at in my array of titles
+    let currentPhraseIndex = 0;
+
+    // Async function so it can await the sleep function created above
+    const writingLoop = async () => {
+        // a while loop to make this function run FOREVER
+        while (true) {
+            // get access to the words in my array
+            let currentWord = titles[currentPhraseIndex];
+
+            // loops over every letter in the word
+            for (let i = 0; i < currentWord.length; i++) {
+                // substring is a method that returns the part of the string between the two arguments you pass to it
+                typewriter.innerText = currentWord.substring(0, i + 1)
+                // pass in sleepTime which is defined above
+                await sleep(sleepTime);
+            }
+
+            // pause before next word for sleepTime x 5
+            await sleep(sleepTime * 5);
+
+            // start at last letter of the word and loop backwards to nothing
+            for (let i = currentWord.length; i > 0; i--) {
+                typewriter.innerText = currentWord.substring(0, i - 1)
+                await sleep(sleepTime);
+            }
+
+            // a little more sleepy time
+            await sleep(sleepTime);
+
+            // switching words
+            if (currentPhraseIndex === titles.length - 1) {
+                currentPhraseIndex = 0;
+            } else {
+                currentPhraseIndex++;
+            }
+        }
+    }
+
+    // Calling my async function
+    writingLoop();
 }
 
 
