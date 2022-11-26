1253f8a386cc5c567edb4dcf7201fde681f86624
MandeepMann <mandeep_mann30@hotmail.com>
2022-11-25 19:33:55 -0500

    playgin around with sticky nav and centering header text

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ portfolio.projects = () => {
     const hoverInfo = document.querySelectorAll()
 }
 
+// Sticky Nav on scroll
+// https://www.youtube.com/watch?v=1cYzyVJbfuM
+portfolio.stickyHeader = () => {
+    const nav = document.querySelector('.mainNav');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > nav.offsetHeight + 150) {
+            nav.classList.add('active')
+        } else {
+            nav.classList.remove('active')
+        }
+    })
+}
+
+// portfolio.stickyHeader = () => {
+//     const nav = document.querySelector('nav')
+
+//     window.addEventListener('scroll', () => {
+//         nav.classList.toggle('stickyNav', window.scrollY > 0)
+//     })
+    
+
+// }
+
+
 // "Get In Touch" button in header that scrolls down to contact secion
 // https://stackoverflow.com/questions/24739126/scroll-to-a-specific-element-using-html
 portfolio.toContact = () => {
@@ -21,8 +46,8 @@ portfolio.toContact = () => {
 }
 
 // Chevron pointing down button that scrolls down to next section (about)
-portfolio.scrollDown = () => {
-    const headerScroll = document.querySelector('.scrollDown')
+portfolio.scrollToAbout = () => {
+    const headerScroll = document.querySelector('.scrollToAbout')
     const aboutSection = document.querySelector('#about')
 
     headerScroll.addEventListener('click', () => {
@@ -37,7 +62,8 @@ portfolio.scrollDown = () => {
 
 
 portfolio.init = () => {
-    portfolio.scrollDown();
+    portfolio.stickyHeader();
+    portfolio.scrollToAbout();
     portfolio.toContact();
 }
 
